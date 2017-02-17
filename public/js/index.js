7c085a085316381718272022ef050ec6ff29724b
FMcIntosh <frasermcintosh1@gmail.com>
2017-02-16 23:52:50 -0800

    check checkboxes

--- public/js/index.js
+++ public/js/index.js
@@ -21,6 +21,50 @@ var data = {
 	]
 }
 
+var formValues = JSON.parse(localStorage.getItem('formValues')) || {};
+var $checkboxes = $("#checkbox-container :checkbox");
+var $button = $("#checkbox-container button");
+
+function allChecked(){
+  return $checkboxes.length === $checkboxes.filter(":checked").length;
+}
+
+function updateButtonStatus(){
+  $button.text(allChecked()? "Uncheck all" : "Check all");
+}
+
+function handleButtonClick(){
+  $checkboxes.prop("checked", allChecked()? false : true)
+}
+
+function updateStorage(){
+  $checkboxes.each(function(){
+    formValues[this.id] = this.checked;
+  });
+
+  formValues["buttonText"] = $button.text();
+  localStorage.setItem("formValues", JSON.stringify(formValues));
+}
+
+$button.on("click", function() {
+  handleButtonClick();
+  updateButtonStatus();
+  updateStorage();
+});
+
+$checkboxes.on("change", function(){
+  updateButtonStatus();
+  updateStorage();
+});
+
+// On page load
+$.each(formValues, function(key, value) {
+  $("#" + key).prop('checked', value);
+});
+
+$button.text(formValues["buttonText"]);
+
+
 var cardIndex = 0;
 
 $(document).ready(function() {
