73c5d2adb92bc6bdf8b721cf0781f63ac1c113f1
FMcIntosh <frasermcintosh1@gmail.com>
2017-02-22 13:32:10 -0800

    Read daily task from data file

--- routes/list.js
+++ routes/list.js
@@ -1,4 +1,10 @@
+var data = require('../task.json');
+var tasks = data.tasks
+var index = Math.floor(Math.random()*tasks.length);
+
+console.log(index);
+var task = tasks[index];
 exports.view = function(req, res){
 
-    res.render('list');
+    res.render('list', task);
 };
