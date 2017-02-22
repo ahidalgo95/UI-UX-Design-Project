43998f13d0ed8142db8ca57ce44b27e308ee907b
Kate Van Donge <katemvd@gmail.com>
2017-02-21 18:34:34 -0800

    scroll down

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,5 @@ var helper = require('../public/js/helper.js');
 
 
 exports.view = function(req, res){
-  console.log(req.query);
-  var index = helper.getIndex();
-
-  var list = data.lists[index];
-  res.render('index', {
-    index: index,
-    list: list
-  });
+  res.render('index', data);
 }
