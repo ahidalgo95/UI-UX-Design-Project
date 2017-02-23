334f6f6622ea2f8de819b5caaee77de9669443a0
Kate Van Donge <katemvd@gmail.com>
2017-02-23 14:58:10 -0800

    stuff on homescreen

--- routes/list.js
+++ routes/list.js
@@ -1,25 +1,21 @@
 var question = require('../task.json');
 var data = require('../data.json');
 //var tasks = question;
-var index = 3;//Math.floor(Math.random()*question.length);
+var index = 1;//Math.floor(Math.random()*question.length);
 
-var task = question[index];
-
+var task = question.tasks[index].task;
 exports.view = function(req, res){
     res.render('list', task);
 };
 
 exports.addAnswer = function(req, res) { 
-  console.log(data);
-  var title = "hi";
-  var date = "today";
+  var det = new Date();
+  var title = task;
+  var date = det.toDateString();
   var items = req.query.name;
   var emoji = "emoj";
   var weather = "weather";
   var userobj = {"title": title, "date": date, "items": items, "emoji": emoji, "weather": weather};
-  console.log(userobj);
-  data.lists.push(userobj);
-  console.log(data);
-
+  data.lists.unshift(userobj);
   res.render('list', data);
  }
