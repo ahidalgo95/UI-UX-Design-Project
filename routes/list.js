335a7d616507c90b130bb53ecee1878c0e4ee7a1
ahidalgo95 <ahidalgo@ucsd.edu>
2017-02-23 16:56:25 -0800

    changes to list

--- routes/list.js
+++ routes/list.js
@@ -1,4 +1,9 @@
-exports.view = function(req, res){
 
-    res.render('list');
+var data= require('../questions.json');
+
+var helper = require('../public/js/helper.js');
+
+
+exports.view = function(req, res){
+    res.render('list', data);
 };
