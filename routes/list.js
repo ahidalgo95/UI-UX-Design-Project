363f97e344229d06c920b5b661cee38c0da88334
ahidalgo95 <ahidalgo@ucsd.edu>
2017-02-23 17:05:33 -0800

    hello

--- routes/list.js
+++ routes/list.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
 
 var data= require('../questions.json');
-
 var helper = require('../public/js/helper.js');
-
-
-exports.view = function(req, res){
-    res.render('list', data);
-=======
-var data = require('../task.json');
-var tasks = data.tasks
+var tasks = data.prompts; 
 var index = Math.floor(Math.random()*tasks.length);
 
 console.log(index);
 var task = tasks[index];
 exports.view = function(req, res){
 
-    res.render('list', task);
->>>>>>> 4ba0bb437c6954c8bad1225ab349a6af4252fcb5
+exports.view = function(req, res){
+    res.render('list', tasks);
 };
